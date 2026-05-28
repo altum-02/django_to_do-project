b0d6931617754954cb27249ca49dbba81e05c159
ALTUM-02 <martin249jesus@gmail.com>
2026-05-28 16:21:39 +0300

    new change

--- frontend/src/types/auth.ts
+++ frontend/src/types/auth.ts
@@ -3,6 +3,13 @@ export interface LoginData {
   password: string;
 }
 
+export interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+}
+
 export interface TokenResponse {
   access: string;
   refresh: string;

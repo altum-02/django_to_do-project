c02188104e69564049f04ab376369efca5c87095
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 12:44:05 +0300

    new change

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
+
+      <h1 className="text-white text-5xl font-bold">
+
+        Django Todo Frontend Working
+
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;

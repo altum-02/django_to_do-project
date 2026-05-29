968cec0baaf0cd70a914dc2f49a6b46ffad50341
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 18:20:06 +0300

    new change

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+
+  return (
+
+    <div className="p-10">
+
+      <h1 className="text-5xl font-bold">
+
+        Welcome to Django Todo App
+
+      </h1>
+
+    </div>
+  );
+};
+
+export default Home;

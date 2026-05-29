75227cb5ce194eda3bf8fe214cd36865d9720da2
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 08:00:34 +0300

    new change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,7 @@
+import Register from "./pages/Register";
+
 function App() {
-  return (
-    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
-
-      <h1 className="text-white text-5xl font-bold">
-        React App Working
-      </h1>
-
-    </div>
-  );
+  return <Register />;
 }
 
 export default App;

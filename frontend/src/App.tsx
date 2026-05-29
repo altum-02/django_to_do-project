bb8306f56ac5866cb02d15c60b170de9c646042c
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 21:04:17 +0300

    new hange

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-import AppRoutes
-from "./routes/AppRoutes";
-
 function App() {
 
-  return <AppRoutes />;
+  return (
+
+    <div className="bg-blue-600 min-h-screen flex items-center justify-center">
+
+      <h1 className="text-white text-5xl font-bold">
+
+        React + TypeScript + Tailwind
+
+      </h1>
+
+    </div>
+  );
 }
 
 export default App;

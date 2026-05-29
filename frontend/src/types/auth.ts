f94be284979c8c4384dc5e421c8e91749d9cffd5
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 14:31:12 +0300

    neew wchage

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,15 @@
+export interface LoginData {
+  username: string;
+  password: string;
+}
+
+export interface RegisterData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface TokenResponse {
+  access: string;
+  refresh: string;
+}

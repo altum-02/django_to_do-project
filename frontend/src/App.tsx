c23c8ab4a28cdcb01747f1cf25a0e9d5084171b1
ALTUM-02 <martin249jesus@gmail.com>
2026-05-29 07:56:52 +0300

    new change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,12 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
 function App() {
-
   return (
-    <BrowserRouter>
+    <div className="bg-blue-500 min-h-screen flex items-center justify-center">
 
-      <Routes>
+      <h1 className="text-white text-5xl font-bold">
+        React App Working
+      </h1>
 
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
+    </div>
   );
 }
 

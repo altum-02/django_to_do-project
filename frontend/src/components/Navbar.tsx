006cfa51c2cc6c77c3a191ac16e65609e36d4e77
ALTUM-02 <martin249jesus@gmail.com>
2026-05-30 09:17:12 +0300

    new change

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -53,8 +53,4 @@ const Navbar = () => {
             </Link>
 
             <button
-              onClick={logout}
-              className="bg-red-500 px-4 py-2 rounded-lg"
-            >
-              Logout
-            <
+              onClick={logo

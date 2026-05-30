c4d173422a89c88c0b3193f29a4afa2be1588767
ALTUM-02 <martin249jesus@gmail.com>
2026-05-30 12:28:11 +0300

    nnew change

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,29 +1,9 @@
-<div className="min-h-screen bg-slate-100 flex items-center justify-center">
+const Login = () => {
+  return (
+    <div>
+      Login Page
+    </div>
+  );
+};
 
-  <div className="bg-white shadow-2xl rounded-3xl p-10 w-full max-w-md">
-
-    <h1 className="text-4xl font-bold mb-8 text-center">
-      Welcome Back
-    </h1>
-
-    <input
-      type="text"
-      placeholder="Username"
-      className="w-full border p-4 rounded-xl mb-4"
-    />
-
-    <input
-      type="password"
-      placeholder="Password"
-      className="w-full border p-4 rounded-xl mb-4"
-    />
-
-    <button
-      className="w-full bg-blue-600 text-white py-4 rounded-xl"
-    >
-      Login
-    </button>
-
-  </div>
-
-</div>
+export default Login;
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -7,7 +7,7 @@ import {
 import Navbar from "../components/Navbar";
 
 import Home from "../pages/Home";
-import Login from "../pages/Login";
+import  Login  from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
 
